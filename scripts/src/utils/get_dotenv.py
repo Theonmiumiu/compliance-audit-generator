da46e51a1aa1c79b60f20191591b9d8a18c26046
zhaoxinhan <zhaoxinhan@memect.co>
2026-03-26 09:14:29 +0800

    修正了一下.env.example

--- scripts/src/utils/get_dotenv.py
+++ scripts/src/utils/get_dotenv.py
@@ -12,18 +12,10 @@ class Config:
     LLM_URL = os.getenv('LLM_URL')
     METHOD_MODEL = os.getenv('METHOD_MODEL')
     RERANK_MODEL = os.getenv('RERANK_MODEL')
-    AUDIO_MODEL = os.getenv('AUDIO_MODEL')
-    AUDIO_MODEL_URL = os.getenv('AUDIO_MODEL_URL')
-    AUDIO_MODEL_API_KEY = os.getenv('AUDIO_MODEL_API_KEY')
 
     EMBEDDING_URL = os.getenv('EMBEDDING_URL')
     EMBEDDING_API_KEY = os.getenv('EMBEDDING_API')
     EMBEDDING_MODEL = os.getenv('EMBEDDING_MODEL')
-
-    OSS_ENDPOINT = os.getenv('OSS_ENDPOINT')
-    TOS_AK = os.getenv('TOS_AK')
-    TOS_SK = os.getenv('TOS_SK')
-    TOS_BUCKET_NAME = os.getenv('TOS_BUCKET_NAME')
 
     DATABASE_HOST = os.getenv('DATABASE_HOST')
     DATABASE_PORT = os.getenv('DATABASE_PORT')
